5ec3858a3bda1628c9b08a37f9b1ba9de468db38
Emanuele Mottola <emanuele.mottola94@gmail.com>
2020-11-26 14:21:06 +0100

    test getEntireCollectionSortedTest working with 3 entries in db

--- test/services/MongoUtilityTest.scala
+++ test/services/MongoUtilityTest.scala
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.mongodb.scala.bson.{BsonBoolean, BsonDateTime, BsonDocument, BsonInt32, BsonString}
+import org.mongodb.scala.bson.{BsonBoolean, BsonDocument, BsonInt32, BsonString}
 
 class MongoUtilityTest {
 
@@ -18,38 +18,66 @@ class MongoUtilityTest {
     val simpleDateFormat: SimpleDateFormat = new SimpleDateFormat(pattern)
 
 
-    val doc1 = BsonDocument(
+    val doc0 = BsonDocument(
       "_id" -> BsonString("0"),
-      "title" -> BsonString("Audi"),
-      "fuel" -> BsonString("diesel"),
-      "price" -> BsonInt32(12000),
+      "title" -> BsonString("Audi A5"),
+      "fuel" -> BsonString("gasoline"),
+      "price" -> BsonInt32(21500),
       "isNew" -> BsonBoolean(false),
-      "mileage" -> BsonInt32(120000),
-      "first_registration" -> BsonDateTime(simpleDateFormat.parse("1994-11-11T23:00:00Z"))
+      "mileage" -> BsonInt32(57000),
+      "firstRegistration" -> "2019-12-20T00:00:00Z"
+    )
+    val doc1 = BsonDocument(
+      "_id" -> BsonString("1"),
+      "title" -> BsonString("Fiat Panda"),
+      "fuel" -> BsonString("gasoline"),
+      "price" -> BsonInt32(6500),
+      "isNew" -> BsonBoolean(false),
+      "mileage" -> BsonInt32(107000),
+      "firstRegistration" -> "2018-11-14T00:00:00Z"
     )
     val doc2 = BsonDocument(
-      "_id" -> BsonString("1"),
-      "title" -> BsonString("BMW"),
-      "fuel" -> BsonString("gasoline"),
-      "price" -> BsonInt32(30000),
-      "isNew" -> BsonBoolean(false),
-      "mileage" -> BsonInt32(1050),
-      "first_registration" -> BsonDateTime(simpleDateFormat.parse("2020-10-08T22:00:00Z"))
-    )
-    val doc3 = BsonDocument(
       "_id" -> BsonString("2"),
-      "title" -> BsonString("AAA"),
-      "fuel" -> BsonString("gasoline"),
-      "price" -> BsonInt32(30000),
+      "title" -> BsonString("BMW X6"),
+      "fuel" -> BsonString("diesel"),
+      "price" -> BsonInt32(13500),
       "isNew" -> BsonBoolean(false),
-      "mileage" -> BsonInt32(1050),
-      "first_registration" -> BsonDateTime(simpleDateFormat.parse("2020-10-08T22:00:00Z"))
+      "mileage" -> BsonInt32(127000),
+      "firstRegistration" -> "2017-01-14T00:00:00Z"
     )
 
-    val mySeq = MongoUtility.getEntireCollectionSorted("_id")
-    assertEquals(mySeq(0), doc1)
-    assertEquals(mySeq(1), doc2)
-    assertEquals(mySeq(2), doc3)
+    val mySeqID = MongoUtility.getEntireCollectionSorted("_id")
+    assertEquals(doc0, mySeqID(0))
+    assertEquals(doc1, mySeqID(1))
+    assertEquals(doc2, mySeqID(2))
+
+    val mySeqTitle = MongoUtility.getEntireCollectionSorted("title")
+    assertEquals(doc0, mySeqTitle(0))
+    assertEquals(doc2, mySeqTitle(1))
+    assertEquals(doc1, mySeqTitle(2))
+
+    val mySeqFuel = MongoUtility.getEntireCollectionSorted("fuel")
+    assertEquals(doc2, mySeqFuel(0))
+
+    val mySeqPrice = MongoUtility.getEntireCollectionSorted("price")
+    assertEquals(doc1, mySeqPrice(0))
+    assertEquals(doc2, mySeqPrice(1))
+    assertEquals(doc0, mySeqPrice(2))
+
+    val mySeqisNew = MongoUtility.getEntireCollectionSorted("isNew")
+    assertEquals(doc0, mySeqisNew(0))
+    assertEquals(doc1, mySeqisNew(1))
+    assertEquals(doc2, mySeqisNew(2))
+
+    val mySeqMileage = MongoUtility.getEntireCollectionSorted("mileage")
+    assertEquals(doc0, mySeqMileage(0))
+    assertEquals(doc1, mySeqMileage(1))
+    assertEquals(doc2, mySeqMileage(2))
+
+    val mySeqfirstRegistration = MongoUtility.getEntireCollectionSorted("firstRegistration")
+    assertEquals(doc2, mySeqfirstRegistration(0))
+    assertEquals(doc1, mySeqfirstRegistration(1))
+    assertEquals(doc0, mySeqfirstRegistration(2))
 
   }
 }
